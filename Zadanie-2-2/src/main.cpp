1665504ee542e879c3654317469dca12e9ab7876
Sebastian <sbeastian.kiszka@student.wat.edu.pl>
2021-02-19 17:25:36 +0100

    Wykonano zadanie 2.2.4 – button

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,44 +1,41 @@
 #include <Arduino.h>
 #include <avr/io.h>
-
-uint8_t ledState[] = {0xFF, 0x7E, 0x3C, 0x18, 0x00, 0x18, 0x3C, 0x7E};
-uint8_t *pLedState = ledState;
+#define SOLUTION 0
+uint8_t button;
 bool state = true;
 
-void togglePinD13(bool state)
+void togglePinD13(bool state) 
 {
   PORTB = (state << 5);
   state = !(state);
 }
 
-void delay()
-{
-  for (uint32_t j = 0x2FFFF; j > 0; j--)
-  {
+void delay() {
+  for (uint32_t j = 0x1FFFF; j > 0; j--)
     asm volatile("nop");
-  }
 }
 
-int main()
+int main() 
 {
+  DDRB &= !(1 << 0);
   DDRB |= (1 << 5);
-  DDRD |= 0xFF;
   while (1)
+{
+#if (SOLUTION == 0)
+  button = (PINB & (1 << PINB0));
+  if (button == 0)
   {
     togglePinD13(&state);
     delay();
-    for (uint8_t i = 0; i < sizeof(ledState); i++)
-    {
-      PORTD = ledState[i];
-      delay();
-    }
-    pLedState = ledState;
-    for (uint8_t i = 0; i < sizeof(ledState); i++)
-    {
-      PORTD = *pLedState;
-      pLedState++;
-      delay();
-    }
   }
+  else
+    delay();
+#elif (SOLUTION == 1)
+  while (!(PINB & (1 << PINB0))){
+  togglePinD13(&state);
+  delay();
+}
+#endif
+}
 }
   
