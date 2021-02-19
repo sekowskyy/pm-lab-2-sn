4d37e319e8f8e42a01d3a98868ab24ea58908986
Sebastian <sbeastian.kiszka@student.wat.edu.pl>
2021-02-19 17:17:32 +0100

    Wykonano zadanie 2.2.3 – wskaźniki

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,23 +1,43 @@
 #include <Arduino.h>
 #include <avr/io.h>
-#define LED_LENGTH 8
+
+uint8_t ledState[] = {0xFF, 0x7E, 0x3C, 0x18, 0x00, 0x18, 0x3C, 0x7E};
+uint8_t *pLedState = ledState;
+bool state = true;
+
+void togglePinD13(bool state)
+{
+  PORTB = (state << 5);
+  state = !(state);
+}
+
+void delay()
+{
+  for (uint32_t j = 0x2FFFF; j > 0; j--)
+  {
+    asm volatile("nop");
+  }
+}
 
 int main()
 {
-  DDRD |= 0xFF; 
+  DDRB |= (1 << 5);
+  DDRD |= 0xFF;
   while (1)
-  { 
-    for (uint8_t i = 0; i < LED_LENGTH; i++)  
+  {
+    togglePinD13(&state);
+    delay();
+    for (uint8_t i = 0; i < sizeof(ledState); i++)
     {
-      PORTD = (1 << i);
-      for (uint32_t j = 0x1FFFF; j > 0; j--)
-      {
-        __asm__ __volatile__("nop");
-      }
+      PORTD = ledState[i];
+      delay();
     }
-    for (uint32_t j = 0x1FFFF; j > 0; j--)
+    pLedState = ledState;
+    for (uint8_t i = 0; i < sizeof(ledState); i++)
     {
-      __asm__ __volatile__("nop");
+      PORTD = *pLedState;
+      pLedState++;
+      delay();
     }
   }
 }
